b1443f1d50790d68a67e545435ef84272fb8c976
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-03-29 19:13:41 +0530

    Trying out a focused approach to displaying screenshots

--- AndroidDevelopment/MovieApp/app/src/main/java/com/example/movieapp/ui/widgets/ScreenShotRow.kt
+++ AndroidDevelopment/MovieApp/app/src/main/java/com/example/movieapp/ui/widgets/ScreenShotRow.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,19 +20,33 @@ import com.example.movieapp.R
 fun ScreenShotRow(
     list: List<String> = emptyList()
 ) {
+    val focusedIndex = remember {
+        mutableIntStateOf(0)
+    }
     LazyRow {
         items(10) {
-            ScreenShotImage()
+            ScreenShotImage(
+                index = it,
+                focusedIndex = focusedIndex
+            )
         }
     }
 }
 
 @Composable
 private fun ScreenShotImage(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    index: Int,
+    focusedIndex: MutableState<Int>
 ) {
     Image(
-        modifier = modifier.padding(6.dp).clip(RoundedCornerShape(8.dp)),
+        modifier = modifier
+            .padding(6.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .onFocusEvent {
+                event ->
+                          if (event.isFocused) focusedIndex.value = index
+            },
         painter = painterResource(id = R.drawable.screenshot),
         contentDescription = null)
 }

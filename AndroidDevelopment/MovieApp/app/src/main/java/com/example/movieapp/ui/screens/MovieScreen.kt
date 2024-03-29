d386f938bc7bcb3831e1a1333342ec170840a82e
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-03-29 22:10:09 +0530

    Added TopNavBar and a preview for the movie screen

--- AndroidDevelopment/MovieApp/app/src/main/java/com/example/movieapp/ui/screens/MovieScreen.kt
+++ AndroidDevelopment/MovieApp/app/src/main/java/com/example/movieapp/ui/screens/MovieScreen.kt
@@ -5,19 +5,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.movieapp.data.Movie
+import com.example.movieapp.data.dummyMovie
+import com.example.movieapp.ui.screens.components.MainScreenBody
 import com.example.movieapp.ui.screens.components.MovieScreenBody
+import com.example.movieapp.ui.widgets.TopNavBar
 
 @Composable
 fun MovieScreen (
     modifier: Modifier = Modifier,
     navController: NavController,
-    movie: Movie
+    movie: Movie = dummyMovie
 ) {
     Surface {
         Scaffold (
-            topBar = {},
+            topBar = {
+                 TopNavBar(onBackClick = {
+                     navController.popBackStack()
+                 })
+            },
             bottomBar = {}
         ) {
             Surface (
@@ -29,4 +38,10 @@ fun MovieScreen (
             }
         }
     }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun PreviewMovieScreen() {
+    MovieScreen(navController = rememberNavController())
 }
